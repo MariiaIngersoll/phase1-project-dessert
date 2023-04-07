b850880bf511487ad965e471d9f82830faf79403
+ <+>
2023-04-07 11:06:33 -0400

    created handleSubmit function that is supposed to show a selected dessert information

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // declaring Global variables
 
-//grab div that i am gonna append the dessert info to
+//grabs div that i am gonna append the dessert info to
 const desertInfoAppend = document.getElementById("results")
-//grabs the form form
-const dropdown = document.getElementById("dropdown")
+//grabs the form 
+const form = document.getElementById("dropdown")
 //grabs the select menu
-const selectMenu = document.getElementById("nameOfDrink")
+const selectMenu = document.getElementById("nameOfDessert")
+let desserts = []
 
 document.addEventListener("DOMContentLoaded", () => {
     handleLoad()
+    form.addEventListener("submit", handleSubmit)
 })
 
 // fetching info to my select dropdown menu
@@ -16,25 +18,30 @@ const fetchURL = 'http://localhost:3000/desserts'
 function handleLoad() {
     fetch(fetchURL)
         .then(res => res.json())
-        .then(data => data.forEach(dessert => {
-            appendInfoToOption(dessert)
-        }))};
+        .then(data => {
+            data.forEach(dessert => {
+                appendInfoToOption(dessert)
+            })
+            desserts = data
+         } )};
 
+function handleSubmit(e){
+    e.preventDefault()
+    //create a variable that captures the value of each option 
+    const dessertName = e.target.select.value
+    desserts.find(cake => {
+        cake.name = dessertName
+        showDessertsInfo(cake)
+        }
+    )
+}
     //appends a dessert name to the options list
 function appendInfoToOption(dessert) {
         const newOption = document.createElement("option")
         newOption.innerHTML = dessert.name
         selectMenu.appendChild(newOption)
-        dropdown.addEventListener("submit", (e) => {
-            e.preventDefault()
-        //create a variable that captures the value of each option 
-            const valueOfSelectedOption = newOption.value
-
-            if (dessert.name === valueOfSelectedOption){
-                showDessertsInfo(dessert)
-                console.log(dessert)
-            }
-    })
+        
+}
 // get the value of each option 
 function showDessertsInfo(cake) {
         const img = document.createElement("img")
@@ -44,4 +51,3 @@ function showDessertsInfo(cake) {
         desertInfoAppend.append(h2, img)
 
     }
-}
