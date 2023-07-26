38f9af1bfa385004d3a565e32b7594f74d10a0d2
+ <+>
2023-07-26 00:00:46 -0400

    updated my css code, added instructions for each recipy

--- script.js
+++ script.js
@@ -1,96 +1,87 @@
-// declaring Global variables
-
-//grabs div that is going to append the dessert`s information
-const divWithDessertInfo = document.getElementById("results")
-//grabs the form 
-const form = document.getElementById("dropdown")
-//grabs the select menu
-const selectMenu = document.getElementById("nameOfDessert")
-//create variable that stores data we got from fetch
-let desserts = []
+const dropdown = document.getElementById("dropdown");
+const dessertInfo = document.querySelector(".dessert-info");
+let desserts = [];
 
 document.addEventListener("DOMContentLoaded", () => {
-    handleLoad()
-    form.addEventListener("submit", handleSubmit)
-})
+    handleLoad();
+});
+
+dropdown.addEventListener("change", handleChange);
 
 // fetching info to my select dropdown menu
-const fetchURL = 'http://localhost:3000/desserts'
+const fetchURL = 'http://localhost:3000/desserts';
 function handleLoad() {
     fetch(fetchURL)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(dessert => {
-                appendInfoToOption(dessert)
-            })
-            desserts = data
-         } )};
-//function that handles the submit event
-function handleSubmit(e){
-    //prevents page from refreshing 
-    e.preventDefault()
-    //create a variable that captures the value of each option 
-    const dessertName = e.target.select.value
-    console.log(dessertName)
-    desserts.find(cake => {
-       if (cake.name === dessertName){
-        showDessertsInfo(cake)
-        }}
-    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        data.forEach(dessert => {
+          appendInfoToOption(dessert);
+        });
+        desserts = data;
+      });
 }
-    //appends a dessert name to the options list
+
 function appendInfoToOption(dessert) {
-        const newOption = document.createElement("option")
-        newOption.innerHTML = dessert.name
-        selectMenu.appendChild(newOption)
-        
+    const newOption = document.createElement("option");
+    newOption.innerHTML = dessert.name;
+    dropdown.appendChild(newOption);
 }
-// appending the value of each option to the page
-function showDessertsInfo(cake) {
-       // adds an image for a dessert 
-        const img = document.createElement("img")
-        img.src = cake.pictureURL
-       //adds a dessert name 
-        const h2 = document.createElement("h2")
-        h2.textContent = cake.name
-       //create heading for Ingridients
-        const h4 = document.createElement('h4')
-        h4.innerText= "INGREDIENTS:"
-        //creates a list of ingredients as li elements 
-        const ul = document.createElement("ul")
-        const ingredients = cake.ingridients
-        ingredients.forEach(item => {
-            const li = document.createElement("li")
-            li.textContent = item
-            ul.appendChild(li)
 
-        //creates a paragraph element that holds dessert likes  
-        const likePar = document.createElement('p')
-        likePar.innerText = `${cake.amountOfLikes} likes`
+function handleChange(e) {
+    const dessertName = e.target.value;
+    if (dessertName === "option1") {
+        dessertInfo.textContent = "";
+    } else {
+    desserts.find(cake => {
+        if (cake.name === dessertName) {
+            showDessertsInfo(cake);
+        }
+    })
+};
+}
 
-        //creates like button 
-        const btn = document.createElement('button')
-        btn.setAttribute("id", "likeBtn")
-        btn.innerText = "LIKE THIS DESSERT "
-        btn.append(likePar)
-        //i will add the instructions for each dessert once i figure out how to do it in css
+function showDessertsInfo(dessert) {
+    const img = document.createElement("img");
+    img.src = dessert.pictureURL;
 
-        //code below adds an instruction for each dessert
-        // const h5 = document.createElement("h5")
-        // h5.textContent = cake.instruction
+    const h2 = document.createElement("h2");
+    h2.textContent = dessert.name;
 
-        divWithDessertInfo.textContent = ""
+    const h4 = document.createElement('h4');
+    h4.innerText = "INGREDIENTS:";
+    const ul = document.createElement("ul");
+    const ingredients = dessert.ingridients;
+    ingredients.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        ul.appendChild(li);
+    });
 
-        divWithDessertInfo.append(h2, img, h4, ul, btn)
+    const h5 = document.createElement("h5");
+    h5.innerText = "INSTRUCTIONS";
+    const ol = document.createElement("ol");
+    const instructions = dessert.instruction;
+    instructions.forEach(instr => {
+        const li1 = document.createElement("li");
+        li1.textContent = instr;
+        ol.appendChild(li1);
+    });
 
-        img.addEventListener("mouseover", () => img.setAttribute("style", "box-shadow: 10px 20px #c35c88"))
-        img.addEventListener("mouseout",()=> img.setAttribute("style", "box-shadow: 0 0"))
+    const likePar = document.createElement('p');
+    likePar.innerText = `${dessert.amountOfLikes} likes`;
+    const btn = document.createElement('button');
+    btn.setAttribute("id", "likeBtn");
+    btn.innerText = "LIKE THIS DESSERT";
+    btn.append(likePar);
 
-        //event listener for the Like Button. 
-        btn.addEventListener('click', () => {
-            //every time you click on the like button the number will go up by 1 
-            cake.amountOfLikes += 1;
-            likePar.innerText = `${cake.amountOfLikes} likes`
-            })
-        })
- }
+    dessertInfo.textContent = "";
+    dessertInfo.append(h2, img, h4, ul, h5, ol, btn);
+    img.addEventListener("mouseover", () => img.setAttribute("style", "box-shadow: 10px 10px #dd6248;"));
+    img.addEventListener("mouseout",()=> img.setAttribute("style", "box-shadow: 0 0"));
+    btn.addEventListener('click', () => {
+        //every time you click on the like button the number will go up by 1
+        dessert.amountOfLikes += 1;
+        likePar.innerText = `${dessert.amountOfLikes} likes`;
+    });
+}
